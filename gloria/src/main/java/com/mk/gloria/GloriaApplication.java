0163345ec9388eeae50cc0e0555c5773d9c8c783
EthanPark93 <katsube01@gmail.com>
2024-01-30 10:40:18 +0900

    init-pjt

---
+++ gloria/src/main/java/com/mk/gloria/GloriaApplication.java
@@ -0,0 +1,13 @@
+package com.mk.gloria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GloriaApplication.class, args);
+	}
+
+}
